7ef263360486fec64245b451c325e0c4b9830de8
uttam kumar patra <dev.uttampatra@gmail.com>
2023-04-26 13:39:02 +0530

    all

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
-const Login = (props) => {
+const Login = () => {
+  const [email, setEmail] = useStste("");
+  const [password, setPassword] = useStste("");
+
+  const signin = (e) => {
+    e.preventDefault();
+  };
+
+  const regester = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -14,19 +25,29 @@ const Login = (props) => {
         <h1>Sign-in</h1>
         <form action="">
           <h5>User</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className="login__signin">Sign in</button>
+          <button type="submit" className="login__signin">
+            Sign in
+          </button>
         </form>
         <p>
           By continuing, you agree to Amazon's Conditions of Use and Privacy
           Notice.
         </p>
 
-        <button className="regester__account">
+        <button onClick={regester} className="regester__account">
           Create your Amazone Account
         </button>
       </div>
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,10 +1,14 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBW9Y1SBWdfFNRwyQYBX_gXdns7gsvWHZ0",
-    authDomain: "e-colne.firebaseapp.com",
-    projectId: "e-colne",
-    storageBucket: "e-colne.appspot.com",
-    messagingSenderId: "169104708707",
-    appId: "1:169104708707:web:0ea0a179a8696275051456",
-    measurementId: "G-K07WHVPBR0"
-  };
+  apiKey: "AIzaSyBW9Y1SBWdfFNRwyQYBX_gXdns7gsvWHZ0",
+  authDomain: "e-colne.firebaseapp.com",
+  projectId: "e-colne",
+  storageBucket: "e-colne.appspot.com",
+  messagingSenderId: "169104708707",
+  appId: "1:169104708707:web:0ea0a179a8696275051456",
+  measurementId: "G-K07WHVPBR0",
+};
+
+const firebase = firebaseinitialize(firebaseConfig);
